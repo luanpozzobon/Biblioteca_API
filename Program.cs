d6b9a39c78533c31261ed778a91f49ccac6eb866
LuanPozzobon <108753073+LuanPozzobon@users.noreply.github.com>
2023-11-17 18:38:33 -0300

    arrumando

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<BibliotecaDbContext>();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
